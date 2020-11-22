09b90bc00cb7c17ad99e50bf453c9b7d899d9183
mingrui-liu <liumingrui1997@gmail.com>
2020-11-22 18:53:38 -0500

    Update generator_fsl.py

--- siamese/generator_fsl.py
+++ siamese/generator_fsl.py
@@ -55,7 +55,81 @@ def create_generators():
 		# train test split 
 		train_paths, test_paths, train_labels, test_labels = train_test_split(all_image_paths, all_labels,test_size = 0.1)
 
+
 		return train_paths, test_paths, train_labels, test_labels
+
+
+	def load_file_from_disk_with_additional_image(data_pat):
+		all_image_paths_1 = sorted(glob.glob(data_path + "image_test/*.jpg"))
+		all_image_paths_2 = sorted(glob.glob(data_path + "Additional_Images/*.jpg"))
+
+		# read the excel file
+		MHPgrades = pd.read_excel(data_path +"MHPgrades.xlsx",header = 0)
+		MHPgrades2 = pd.read_excel(data_path +"SKN-APP-0452-MHPgrades.xlsx",header = 1)
+
+		MHPgrades2 = MHPgrades2.dropna(axis = 0)
+		MHPgrades2 = MHPgrades2.melt(id_vars=["Subject No.", "Side of Face"], 
+		        var_name="VISIT", 
+		        value_name="Mottled hyperpigmentation").sort_values(by = ['Subject No.','Side of Face'])
+
+		grade1 = MHPgrades["Mottled hyperpigmentation"]
+
+		#Manipulate excel file
+		MHPgrades2.loc[MHPgrades2['VISIT'] == 'Baseline' , 'VISIT'] = '00'
+		MHPgrades2.loc[MHPgrades2['VISIT'] == 'Week 4' , 'VISIT'] = '04'
+		MHPgrades2.loc[MHPgrades2['VISIT'] == 'Week 8' , 'VISIT'] = '08'
+		MHPgrades2.loc[MHPgrades2['VISIT'] == 'Week 12' , 'VISIT'] = '12'
+		MHPgrades2.loc[MHPgrades2['VISIT'] == 'Week 16' , 'VISIT'] = '16'
+		MHPgrades2.loc[MHPgrades2['VISIT'] == 'Week 17' , 'VISIT'] = '17'
+
+		MHPgrades2.loc[MHPgrades2['Side of Face'] == 'Left' , 'Side of Face'] = 'L'
+		MHPgrades2.loc[MHPgrades2['Side of Face'] == 'Right' , 'Side of Face'] = 'R'
+
+		#match grade and images and make valid pair
+		valid_path = []
+		valid_label = []
+		for index, row in MHPgrades2.iterrows():
+		    id = str(row['Subject No.'] )
+		    id = id.zfill(4)
+
+		    side = row['Side of Face']
+		   
+		    visit = str(row['VISIT'])
+
+		    grade = row['Mottled hyperpigmentation']
+		  
+
+
+		    output = [path for path in all_image_paths_2 if path.startswith('/content/drive/My Drive/Unilever/Additional_Images/P' + id + '_FACE_' + side + '_S00_VCR_S2_V00D00W' + visit)] 
+		    if len(output)>0:
+		      valid_path.append(output[0])
+		      valid_label.append(grade)
+
+		all_image_paths = all_image_paths_1 + valid_path
+		all_image_labels = list(grade1) +  valid_label
+
+		##Create label and zip dataset
+		all_label = []
+		for grade in all_image_labels:
+		  if grade < 3.0:
+		    label = 2.5
+		  elif grade > 5.0:
+		    label = 5.5
+		  else:
+		    label = grade
+		  all_label.append(label)
+
+		label_to_index = dict((name, index) for index, name in enumerate(sorted(set(all_label))))
+		all_labels = [label_to_index[grade] for grade in all_label]
+
+		##train val test split
+		train_paths, test_paths, train_labels, test_labels = train_test_split(all_image_paths, all_labels, test_size=0.1, random_state=1)
+
+		return train_paths, test_paths, train_labels, test_labels
+
+
+
+
 
 
 	def create_ds(paths,labels):
@@ -88,7 +162,7 @@ def create_generators():
 
 
 
-	train_paths, test_paths, train_labels, test_labels = load_file_from_disk(data_path)
+	train_paths, test_paths, train_labels, test_labels = load_file_from_disk_with_additional_image(data_path)
 	# here's our final training dataset
 	train_ds = create_ds(train_paths,train_labels)
 	test_ds = create_ds(test_paths,test_labels)
